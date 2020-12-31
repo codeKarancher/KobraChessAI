fde08c7bfe3bf23767d662449db5829bca4eddb4
karan <ks920@cam.ac.uk>
2020-12-31 17:50:58 +0530

    Writing Engine class - 01

--- engine.py
+++ engine.py
@@ -4,12 +4,6 @@ from tensorflow import keras
 import numpy as np
 import scipy.optimize
 
-def board_evaluator(board: chess.Board, color: chess.Color) -> float:
-    return 0
-
-def bfs_best_move(board: chess.Board, color: chess.Color) -> chess.Move:
-    return board.generate_legal_moves()[0]
-
 class Evaluator:
 
     def __init__(self, num_inputs: int, hidden_layer_sizes=[32, 32]):
@@ -23,10 +17,43 @@ class Evaluator:
     def __init__(self, model: keras.Model):
         self.model = model
 
-    def func(self, input):
-        return self.model(np.array([input]))
+    def boardToNNRepresentation(self, board: chess.Board):
+        array = np.zeros(64)
+        piecesDict = board.piece_map()
+        for square in piecesDict:
+            array[square] = piecesDict.get(square).piece_type
+        return array
+
+    def func(self, board: chess.Board):
+        input = self.boardToNNRepresentation(board)
+        return self.model(np.array(input))
 
     @classmethod
-    def randomModelFromModel(cls, model: keras.Model, deviation=0.1):
+    def randomModelFromModel(cls, model: keras.Model, deviation=1):
         new_model = keras.models.clone_model(model)
-        new_model.trainabl
+        for layer in new_model.layers:
+            layer.set_weights(np.random.uniform(layer.get_weights() - deviation, layer.get_weights() + deviation))
+        return Evaluator(new_model)
+
+class ColorError(Exception):
+    """Raised if the wrong chess color was detected"""
+    pass
+
+class Engine:
+
+    def __init__(self, evaluator: Evaluator, color: chess.Color):
+        self.evaluator = evaluator
+        self.color = color
+
+    def best_move(self, board: chess.Board):
+        # currently does only one move look-ahead
+        high = -1000000
+        for move in board.legal_moves:
+            board.push(move)
+            rating = self.evaluator.func(board)
+            if rating > high:
+                high = rating
+                best_move = move
+            board.pop(move)
+        return best_move
+
